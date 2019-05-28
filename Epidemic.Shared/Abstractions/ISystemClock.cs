6717664d07169f25fd54052185b246b5637a6beb
Brad Morgan <brad@bradmorgan.co.nz>
2019-05-28 22:57:40 +1200

    Renamed Epidemic.Domain to Epidemic.Shared physically and fixed references.

---
+++ Epidemic.Shared/Abstractions/ISystemClock.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Epidemic.Shared.Abstractions
+{
+    /// <summary>
+    /// Abstracts the system clock to facilitate testing.
+    /// </summary>
+    public interface ISystemClock
+    {
+        /// <summary>
+        /// Gets the current system time in UTC.
+        /// </summary>
+        DateTime UtcNow { get;  }
+    }
+}
